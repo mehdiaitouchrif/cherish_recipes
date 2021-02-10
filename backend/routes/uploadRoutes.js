b861b4663081ceec5b2026dc72b0ac8a2d2291cf
mehdiaitouchrif <mehdi.aitouchrif1@gmail.com>
2021-02-10 16:23:44 +0100

    Implement AWS S3 Bucket in the backend

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,94 +1,33 @@
-// import path from 'path'
-// import express from 'express'
-// import multer from 'multer'
-
-// const router = express.Router()
-
-// const storage = multer.diskStorage({
-// 	destination(req, file, cb) {
-// 		cb(null, 'uploads/')
-// 	},
-// 	filename(req, file, cb) {
-// 		cb(
-// 			null,
-// 			`${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
-// 		)
-// 	},
-// })
-
-// function checkFileType(file, cb) {
-// 	const fileTypes = /jpg|jpeg|png|webp/
-// 	const extname = fileTypes.test(path.extname(file.originalname).toLowerCase())
-// 	const mimetype = fileTypes.test(file.mimetype)
-
-// 	if (extname && mimetype) {
-// 		return cb(null, true)
-// 	} else {
-// 		cb('Please upload a file of image type')
-// 	}
-// }
-
-// const upload = multer({
-// 	storage,
-// 	fileFilter: function (req, file, cb) {
-// 		checkFileType(file, cb)
-// 	},
-// })
-
-// router.post('/', upload.single('profile'), function (req, res) {
-// 	res.send(`/${req.file.path}`)
-// })
-
-// router.post('/', upload.array('recipe', 6), function (req, res) {
-// 	res.send(`/${req.file.path}`)
-// })
-
-// export default router
-
 import express from 'express'
-import multer from 'multer'
+const router = express.Router({ mergeParams: true })
+import aws from 'aws-sdk'
 
-const storage = multer.diskStorage({
-	destination: function (req, file, cb) {
-		cb(null, './uploads')
-	},
-	filename: function (req, file, cb) {
-		cb(null, `${Date.now()}${file.originalname}`)
-	},
-})
-
-const fileFilter = (req, file, cb) => {
-	// reject a file
-	const fileTypes = /jpg|jpeg|png|webp/
-	const mimetype = fileTypes.test(file.mimetype)
-
-	if (mimetype) {
-		cb(null, true)
-	} else {
-		cb(null, false)
+// GET SIGNED S3 URL
+const SignS3 = (req, res) => {
+	const s3 = new aws.S3()
+	const fileName = req.query['file-name']
+	const fileType = req.query['file-type']
+	const s3Params = {
+		Bucket: process.env.S3_BUCKET_NAME,
+		Key: fileName,
+		Expires: 60,
+		ContentType: fileType,
+		ACL: 'public-read',
 	}
+
+	s3.getSignedUrl('putObject', s3Params, (err, data) => {
+		if (err) {
+			console.log(err)
+			return res.end()
+		}
+		const returnData = {
+			signedRequest: data,
+			url: `https://${process.env.S3_BUCKET_NAME}.s3.amazonaws.com/${fileName}`,
+		}
+
+		res.json(returnData)
+	})
 }
 
-const upload = multer({
-	storage,
-	limits: {
-		fileSize: 1024 * 1024 * 2,
-	},
-	fileFilter,
-})
-
-const router = express.Router()
-
-router.post('/recipe', upload.array('recipeImages', 4), (req, res) => {
-	// Construct an arr of filepaths
-
-	const filepaths = []
-	req.files.map((file) => filepaths.push(`/${file.path}`))
-	res.send(filepaths)
-})
-
-router.post('/profile', upload.single('profilePhoto'), (req, res) => {
-	res.send(`/${req.file.path}`)
-})
-
+router.get('/sign-s3', SignS3)
 export default router
